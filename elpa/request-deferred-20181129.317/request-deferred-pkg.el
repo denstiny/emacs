dad602aaf894a5bb077334601d6e4152b6bf74f2
denstiny <2254228017@qq.com>
2021-01-14 16:34:31 +0800

    init config

---
+++ elpa/request-deferred-20181129.317/request-deferred-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/aercn/.emacs.d/elpa/request-deferred-20181129.317/request-deferred.el  -*- no-byte-compile: t -*-
+(define-package "request-deferred" "20181129.317" "Wrap request.el by deferred" '((deferred "0.3.1") (request "0.2.0")) :commit "0183da84cb45eb94da996cd2eab714ef0d7504cc" :authors '(("Takafumi Arakaki <aka.tkf at gmail.com>")) :maintainer '("Takafumi Arakaki <aka.tkf at gmail.com>") :url "https://github.com/tkf/emacs-request")
